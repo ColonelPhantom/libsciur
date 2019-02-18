6746ea2ce98710df79a95fd974690faa11a6c8dc
Quinten Kock <quintenkock@gmail.com>
2019-02-18 12:08:05 +0100

    Move to C++ and add some typedefs

--- storage/sciur-storage.hpp
+++ storage/sciur-storage.hpp
@@ -1,9 +1,14 @@
 #ifndef _SCIUR_STORAGE_H
 #define _SCIUR_STORAGE_H
 
-#include <stdint.h>
+#include <cstdint>
+#include <string>
 
 typedef uint_fast64_t docid_t;
+typedef uint_fast64_t tokid_t;
+
+typedef uint_fast32_t occnum_t;
+typedef uint_fast64_t offset_t;
 
 int setup(char* indexfile);
 int write(docid_t docid,  
